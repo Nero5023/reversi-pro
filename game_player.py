d51d56befc18438144f3c34a288da4247f777061
Nero Zuo <nero5023@gmail.com>
2021-04-25 23:02:44 +0800

    add human_player

--- game_player.py
+++ game_player.py
@@ -2,13 +2,15 @@ from mcts import MCTS
 import config
 from board import GameState
 from nnet import NeuralNet
+from util import position_to_int_move, int_move_to_position
+import numpy as np
 
 
 class GamePlayer:
     def take_move(self, move):
         pass
 
-    def pick_move(self):
+    def pick_move(self, game_state: GameState):
         pass
 
     def rival_take_move(self, move):
@@ -20,7 +22,7 @@ class MCTSPlayer(GamePlayer):
         self.mcts = MCTS(nn)
         self.sum_num = sim_num
 
-    def pick_move(self):
+    def pick_move(self, game_state):
         self.mcts.search(self.sum_num)
         move = self.mcts.pick_move()
         return move
@@ -32,13 +34,38 @@ class MCTSPlayer(GamePlayer):
         self.mcts.take_move(move)
 
 
+class HumanPlayer(GamePlayer):
+    def pick_move(self, game_state: GameState):
+        while True:
+            print("Please input:")
+            print("legal action:")
+            for i in np.where(game_state.get_legal_actions() == 1)[0]:
+                print(int_move_to_position(i))
+            human_input_str = input(">")
+            if human_input_str == "pass":
+                return 64
+            if len(human_input_str) != 2:
+                print("input is two char")
+                continue
+            if not human_input_str[0].isnumeric():
+                continue
+            row = int(human_input_str[0])
+            if row > 8 or row < 0:
+                continue
+            col = human_input_str[1]
+            if ord(col) < ord('A') or ord(col) > ord('H'):
+                continue
+            move = position_to_int_move(human_input_str)
+            return move
+
+
 def play_reversi(player_black: GamePlayer, player_white: GamePlayer, need_print=True):
     game = GameState.INIT_State()
     if need_print:
         print(game.board.to_str())
         print("start")
     while not game.is_terminal:
-        move_black = player_black.pick_move()
+        move_black = player_black.pick_move(game)
         player_black.take_move(move_black)
 
         game = game.take_move(move_black)
@@ -47,7 +74,7 @@ def play_reversi(player_black: GamePlayer, player_white: GamePlayer, need_print=
             print("Black: move {}".format(move_black))
             print(game.board.to_str())
         player_white.rival_take_move(move_black)
-        move_white = player_white.pick_move()
+        move_white = player_white.pick_move(game)
         player_white.take_move(move_white)
 
         game = game.take_move(move_white)
@@ -86,11 +113,18 @@ def play_reversi_benchmark(model_path0, model_path1, nums):
 
 
 if __name__ == '__main__':
-    import os
+    # import os
+    #
+    # os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
+    #
+    #
+    #
+    # play_reversi_benchmark('model_v36.tar', 'model_v10.tar', 10)
 
-    os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
-
-
-
-    play_reversi_benchmark('model_v36.tar', 'model_v10.tar', 10)
-
+    # human_input_str = input(">")
+    # print(human_input_str)
+    nn0 = NeuralNet(config.game_config)
+    nn0.load_checkpoint(filename='model_v.tar')
+    player0 = MCTSPlayer(nn0)
+    human = HumanPlayer()
+    play_reversi(human, player0)
--- util.py
+++ util.py
@@ -7,3 +7,23 @@ class dotdict(dict):
 
 def flatten(t):
     return [item for sublist in t for item in sublist]
+
+
+def int_move_to_position(int_move):
+    if int_move == 64:
+        return "pass"
+    line = int_move // 8
+    col = int_move % 8
+    line += 1
+    col = chr(ord('A') + col)
+    return "{}{}".format(line, col)
+
+
+def position_to_int_move(pos):
+    if pos == 'pass':
+        return 64
+    line = pos[0]
+    col = pos[1]
+    line = int(line) - 1
+    col = ord(col) - ord('A')
+    return 8*line+col
